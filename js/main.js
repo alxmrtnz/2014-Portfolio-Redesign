7c1beb154363a98f54b652de55eed815eaafd2c0
Alex Martinez <amart1892@gmail.com>
2014-03-16 11:50:07 -0400

    javascripting

--- js/main.js
+++ js/main.js
@@ -60,14 +60,21 @@ $( document ).ready(function() {
 
 
 
-    var my_array = new Array();//create an array
+    var sectionArray = new Array();//create an array
     var count = 0;//used for looping through console.log
 
     //loops through each workPiece looking for it's data-color and adds it to the array, also logs it so I can see it working
     $('.workPiece').each(function(){
-        my_array.push($(this).data("color"));
-        console.log(my_array[count]);
+        //my_array.push($(this).data("color"));
+        //console.log(my_array[count]);
         count += 1;
+        console.log('name: ' +$(this).attr('id') + " |  bgcolor: " + $(this).data("color") + " |  height: "+ $(this).outerHeight());
+        var obj = {
+                name: $(this).attr('id'),
+                BGcolor: $(this).data("color"),
+                height: $(this).outerHeight()
+            };
+        sectionArray.push(obj);
     });
 
 
@@ -92,59 +99,66 @@ $( document ).ready(function() {
     console.log("start_pos: " + animation_begin_pos);
     console.log("end_pos: " + animation_end_pos);
 
-
-
+    //COLOR SELECTION
     //$alxBlue: rgb(56, 136, 222)
     //Hands Green: rgb(154, 184, 166)
     // OUMS Blue: rgb(101,196,229)
+    var colors = ['rgb(56, 136, 222)', 'rgb(154, 184, 166)', 'rgb(101,196,229)'];
+    //var beginning_color = new $.Color( colors[0] ); //we can set this here, but it'd probably be better to get it from the CSS; for the example we're setting it here.
+    //var ending_color = new $.Color( colors[1] ) ;//what color we want to use in the end
 
-    var beginning_color = new $.Color( 'rgb(56, 136, 222)' ); //we can set this here, but it'd probably be better to get it from the CSS; for the example we're setting it here.
-    var ending_color = new $.Color( 'rgb(154, 184, 166)' ); ;//what color we want to use in the end
 
-    var colors = ['rgb(154, 184, 166)', 'rgb(101,196,229)'];
+    //console.log("HANDS from top: " + $('.hands').offset().top);
+
+    var colorChange = function(){
+        var beginning_color = new $.Color( colors[sectionNum] );
+        var ending_color = new $.Color( colors[sectionNum+1] );
+
+        console.log('section number: '+sectionNum + "section color: "+colors[sectionNum]);
+
+        scroll_pos = $(this).scrollTop();
+        //console.log("SCROLL POS: " + scroll_pos + " >= " + "ANIMATION BEGIN: " + animation_begin_pos + " && " + scroll_pos + " <= " + animation_end_pos);
+
+         //CHANGING BACKGROUND COLOR
+        if(scroll_pos >= animation_begin_pos && scroll_pos <= animation_end_pos ) {
+              //console.log( 'work scroll position: ' + scroll_pos );
+             //we want to calculate the relevant transitional rgb value
+             var percentScrolled = ((scroll_pos - animation_begin_pos) / ( animation_end_pos - animation_begin_pos ));
+             //console.log("percentScrolled: " + percentScrolled + "scroll position:  " +scroll_pos);
+             var newRed = beginning_color.red() + ( ( ending_color.red() - beginning_color.red() ) * percentScrolled );
+             var newGreen = beginning_color.green() + ( ( ending_color.green() - beginning_color.green() ) * percentScrolled );
+             var newBlue = beginning_color.blue() + ( ( ending_color.blue() - beginning_color.blue() ) * percentScrolled );
+             var newColor = new $.Color( newRed, newGreen, newBlue );
+             //console.log( newColor.red(), newColor.green(), newColor.blue() );
+             $('body').animate({ backgroundColor: newColor }, 0);
+             $('.titleInfo').animate({ backgroundColor: newColor }, 0);
+
+        } else if ( scroll_pos > animation_end_pos ) {
+             $('body').animate({ backgroundColor: ending_color }, 0);
+             $('.titleInfo').animate({ backgroundColor: ending_color }, 0);
+             scroll_pos -= introHeight;
+             sectionNum++;
+             console.log('new section number: '+ sectionNum);
+             return sectionNum;
+        } else if ( scroll_pos < animation_begin_pos ) {
+             $('body').animate({ backgroundColor: beginning_color }, 0);
+             $('.titleInfo').animate({ backgroundColor: beginning_color }, 0);
+        } else { }
+    }
+
+
+
+
 
     /////////////////////////////////////////////////////////////////
 
-
-
     var description = $('.work article.workPiece.current .workPieceContent .description');
+    colorChange();//initial color selection
     $('.current .description').css('color', 'red');;
+    var sectionNum = 0;
     $(document).scroll(function() {
-       //scroll_pos = $(this).scrollTop() - introHeight;
-       scroll_pos = $(this).scrollTop();
-       console.log("SCROLL POS: " + scroll_pos + " >= " + "ANIMATION BEGIN: " + animation_begin_pos + " && " + scroll_pos + " <= " + animation_end_pos);
 
-
-
-
-
-        //CHANGING BACKGROUND COLOR
-       if(scroll_pos >= animation_begin_pos && scroll_pos <= animation_end_pos ) {
-             //console.log( 'work scroll position: ' + scroll_pos );
-            //we want to calculate the relevant transitional rgb value
-            var percentScrolled = ((scroll_pos - animation_begin_pos) / ( animation_end_pos - animation_begin_pos ));
-            //console.log("percentScrolled: " + percentScrolled + "scroll position:  " +scroll_pos);
-            var newRed = beginning_color.red() + ( ( ending_color.red() - beginning_color.red() ) * percentScrolled );
-            var newGreen = beginning_color.green() + ( ( ending_color.green() - beginning_color.green() ) * percentScrolled );
-            var newBlue = beginning_color.blue() + ( ( ending_color.blue() - beginning_color.blue() ) * percentScrolled );
-            var newColor = new $.Color( newRed, newGreen, newBlue );
-            //console.log( newColor.red(), newColor.green(), newColor.blue() );
-            $('body').animate({ backgroundColor: newColor }, 0);
-            $('.titleInfo').animate({ backgroundColor: newColor }, 0);
-
-       } else if ( scroll_pos > animation_end_pos ) {
-            $('body').animate({ backgroundColor: ending_color }, 0);
-            $('.titleInfo').animate({ backgroundColor: ending_color }, 0);
-
-       } else if ( scroll_pos < animation_begin_pos ) {
-            $('body').animate({ backgroundColor: beginning_color }, 0);
-            $('.titleInfo').animate({ backgroundColor: beginning_color }, 0);
-       } else { }
-
-
-
-
-
+       colorChange();
     });
 
 
